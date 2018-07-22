70c981f328b63d012f6a6b0797d273c6edc62a1e
Jed Duffey <jedduffey@gmail.com>
2018-07-21 20:15:05 -0400

    Refactor; extract methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,27 @@
 public class App {
 
-    public static final int SIZE_OF_ARRAY_SQUARE = 11;
+    public static final int SIZE_OF_ARRAY_SQUARE = 5;
 
     public static void main(String[] args) {
 
         PinPadArray pinpad = new PinPadArray(SIZE_OF_ARRAY_SQUARE);
 
-        System.out.println("*** Pinpad App ***");
-        System.out.println("Square Size: " + SIZE_OF_ARRAY_SQUARE);
-        
+        displayIntroMessage();
+
+        displayColumnHeaderUnderscores();
+
+        displayPinpadSquare(pinpad);
+
+    }
+
+    private static void displayColumnHeaderUnderscores() {
         for (int i = 0; i < SIZE_OF_ARRAY_SQUARE; i++) {
             System.out.print("___ ");
         }
-
         System.out.println();
+    }
 
+    private static void displayPinpadSquare(PinPadArray pinpad) {
         for (int index = 0; index < pinpad.getArraySizeOfPinPad(); index++) {
 
             if (pinpad.get(index) < 10) {
@@ -28,7 +35,11 @@ public class App {
                 System.out.println();
             }
         }
+    }
 
+    private static void displayIntroMessage() {
+        System.out.println("*** Pinpad App ***");
+        System.out.println("Square Size: " + SIZE_OF_ARRAY_SQUARE);
     }
 
 }
