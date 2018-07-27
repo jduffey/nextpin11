cb6e32eeb133e1835a069940019cdcc66a219ef0
Jed Duffey <jedduffey@gmail.com>
2018-07-26 21:12:14 -0400

    Change class name

--- src/test/java/SecretNumberTest.java
+++ src/test/java/SecretNumberTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-public class UsageTest {
+public class SecretNumberTest {
 
     @Test
     public void goRightOne() {
@@ -30,26 +30,32 @@ public class UsageTest {
 
     @Test
     public void goLeftOne() {
-//        Customer customer = new Customer();
-//        customer.setAnchorNumber(0);
-//        customer.setDirectionNumber(1);
+        Customer customer = new Customer();
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
 
         // Want to produce:
         // 0 1 2
         // 3 4 5
         // 6 7 8
-//        Pinpad pinpad = new Pinpad(3);
-//        ArrayList customArray1 = new ArrayList();
-//        customArray1.add(1); customArray1.add(0); customArray1.add(2);
-//        customArray1.add(3); customArray1.add(4); customArray1.add(5);
-//        customArray1.add(6); customArray1.add(7); customArray1.add(8);
-//
-//        pinpad.setCustomArray(pinpad.getSquareSize(), customArray1);
+        Pinpad pinpad = new Pinpad(3);
+        ArrayList customArray1 = new ArrayList();
+        customArray1.add(1);
+        customArray1.add(0);
+        customArray1.add(2);
+        customArray1.add(3);
+        customArray1.add(4);
+        customArray1.add(5);
+        customArray1.add(6);
+        customArray1.add(7);
+        customArray1.add(8);
+
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray1);
 
         // From 0 to 1 is "one right"
         // so from the center (4) we move one right
         // therefore secret number is 5
-        //Assert.assertEquals(3, pinpad.getSecretNumber());
+        Assert.assertEquals(3, SecretNumber.findSecretNumber(customer, pinpad));
     }
 
 }
