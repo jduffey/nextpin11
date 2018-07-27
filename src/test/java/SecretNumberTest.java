cce888e14cda33244b0d2099fbb0c18fea04ca8c
Jed Duffey <jedduffey@gmail.com>
2018-07-26 22:12:31 -0400

    Two additional tests for movement within ArrayList bounds

--- src/test/java/SecretNumberTest.java
+++ src/test/java/SecretNumberTest.java
@@ -59,4 +59,66 @@ public class SecretNumberTest {
         Assert.assertEquals(3, SecretNumber.findSecretNumber(customer, pinpad));
     }
 
+    @Test
+    public void goDownOneMovementWithinArrayBounds() {
+        Customer customer = new Customer();
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+
+        // Want to produce:
+        // 4 0 2
+        // 3 1 5
+        // 6 7 8
+        Pinpad pinpad = new Pinpad(3);
+        ArrayList customArray1 = new ArrayList();
+        customArray1.add(4);
+        customArray1.add(0);
+        customArray1.add(2);
+        customArray1.add(3);
+        customArray1.add(1);
+        customArray1.add(5);
+        customArray1.add(6);
+        customArray1.add(7);
+        customArray1.add(8);
+
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray1);
+
+        // From 0 to 1 is "one down"
+        // so from the center (1) we move one down
+        // therefore secret number is 7
+        App.displayPinpadSquare(pinpad);
+        Assert.assertEquals(7, SecretNumber.findSecretNumber(customer, pinpad));
+    }
+
+    @Test
+    public void goUpOneMovementWithinArrayBounds() {
+        Customer customer = new Customer();
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+
+        // Want to produce:
+        // 4 2 1
+        // 3 5 0
+        // 6 7 8
+        Pinpad pinpad = new Pinpad(3);
+        ArrayList customArray1 = new ArrayList();
+        customArray1.add(4);
+        customArray1.add(2);
+        customArray1.add(1);
+        customArray1.add(3);
+        customArray1.add(5);
+        customArray1.add(0);
+        customArray1.add(6);
+        customArray1.add(7);
+        customArray1.add(8);
+
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray1);
+
+        // From 0 to 1 is "one up"
+        // so from the center (5) we move one up
+        // therefore secret number is 2
+        App.displayPinpadSquare(pinpad);
+        Assert.assertEquals(2, SecretNumber.findSecretNumber(customer, pinpad));
+    }
+
 }
