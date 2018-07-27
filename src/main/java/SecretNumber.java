7445a336e766e2184704d06184e2600000182bab
Jed Duffey <jedduffey@gmail.com>
2018-07-26 23:06:20 -0400

    Refactor

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -2,19 +2,8 @@ public class SecretNumber {
 
     public static int findSecretNumber(Customer customer, Pinpad pinpad) {
 
-        int anc = customer.getAnchorNumber();
-        int dir = customer.getDirectionNumber();
-
-        int posAnc = pinpad.getPosOfValue(anc);
-        int posDir = pinpad.getPosOfValue(dir);
-
-        int secretNumber = getSecretNumber(pinpad, posAnc, posDir);
-
-        return secretNumber;
-
-    }
-
-    private static int getSecretNumber(Pinpad pinpad, int posAnc, int posDir) {
+        int posAnc = pinpad.getPosOfValue(customer.getAnchorNumber());
+        int posDir = pinpad.getPosOfValue(customer.getDirectionNumber());
 
         int movementNumber;
 
@@ -28,5 +17,6 @@ public class SecretNumber {
         int positionOfMiddleOfPinpadSquare = pinpad.getArraySizeOfPinPad() / 2;
 
         return pinpad.get(positionOfMiddleOfPinpadSquare + movementNumber);
+
     }
 }
