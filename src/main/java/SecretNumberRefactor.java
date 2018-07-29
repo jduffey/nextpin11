41a5063867fe984ddfaf23a17a3f30ebd5201793
Jed Duffey <jedduffey@gmail.com>
2018-07-29 12:25:29 -0400

    Vertical movement works independently

--- src/main/java/SecretNumberRefactor.java
+++ src/main/java/SecretNumberRefactor.java
@@ -2,6 +2,28 @@ public class SecretNumberRefactor {
 
     public static int findSecretNumber(Customer customer, Pinpad pinpad) {
 
+        int elementOfTheColumnAdjustmentFromTheMiddleNumber = applyHorizontalMovement(customer, pinpad) % pinpad.getArraySizeOfPinPad();
+        int elementOfTheRowAdjustmentFromTheMiddleNumber = applyVerticalMovement(customer, pinpad) % pinpad.getArraySizeOfPinPad();
+
+        return elementOfTheRowAdjustmentFromTheMiddleNumber;
+
+
+    }
+
+    private static int applyVerticalMovement(Customer customer, Pinpad pinpad) {
+
+        int sqrSiz = pinpad.getSquareSize();
+        int posAnc = pinpad.getPosOfValue(customer.getAnchorNumber());
+        int posDir = pinpad.getPosOfValue(customer.getDirectionNumber());
+        int posMid = pinpad.getArraySizeOfPinPad() / 2;
+
+
+        return (pinpad.getArraySizeOfPinPad() + (posMid + ((posDir - posAnc) / sqrSiz * sqrSiz)));
+
+    }
+
+    private static int applyHorizontalMovement(Customer customer, Pinpad pinpad) {
+
         int sqrSiz = pinpad.getSquareSize();
         int posAnc = pinpad.getPosOfValue(customer.getAnchorNumber());
         int posDir = pinpad.getPosOfValue(customer.getDirectionNumber());
--- src/test/java/SecretNumberRefactorTest.java
+++ src/test/java/SecretNumberRefactorTest.java
@@ -66,4 +66,65 @@ public class SecretNumberRefactorTest {
         Assert.assertEquals(23, SecretNumberRefactor.findSecretNumber(customer, pinpad));
     }
 
+    @Test
+    public void verticalMovementOnlyShouldWorkProperly(){
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(i);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(7);
+        Assert.assertEquals(31, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(14);
+        Assert.assertEquals(38, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(21);
+        Assert.assertEquals(45, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(28);
+        Assert.assertEquals(3, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(35);
+        Assert.assertEquals(10, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(42);
+        Assert.assertEquals(17, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(41);
+        Assert.assertEquals(17, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(34);
+        Assert.assertEquals(10, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(27);
+        Assert.assertEquals(3, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(20);
+        Assert.assertEquals(45, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(13);
+        Assert.assertEquals(38, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+
+        customer.setAnchorNumber(48);
+        customer.setDirectionNumber(6);
+        Assert.assertEquals(31, SecretNumberRefactor.findSecretNumber(customer, pinpad));
+    }
+
 }
