43a576fe2c86bccf1947aab374f06728c87277a2
Jed Duffey <jedduffey@gmail.com>
2018-07-29 23:06:13 -0400

    We have a winner! Works as intended; added several validation tests

--- src/main/java/SecretNumberRefactor2.java
+++ src/main/java/SecretNumberRefactor2.java
@@ -2,11 +2,11 @@ public class SecretNumberRefactor2 {
 
     public static int getHorMove(Customer customer, Pinpad pinpad) {
 
-        return customer.getDirectionNumber() % pinpad.getSquareSize() - customer.getAnchorNumber() % pinpad.getSquareSize();
+        return pinpad.getPosOfValue(customer.getDirectionNumber()) % pinpad.getSquareSize() - pinpad.getPosOfValue(customer.getAnchorNumber()) % pinpad.getSquareSize();
     }
 
     public static int getVerMove(Customer customer, Pinpad pinpad) {
-        return customer.getDirectionNumber() / pinpad.getSquareSize() - customer.getAnchorNumber() / pinpad.getSquareSize();
+        return pinpad.getPosOfValue(customer.getDirectionNumber()) / pinpad.getSquareSize() - pinpad.getPosOfValue(customer.getAnchorNumber()) / pinpad.getSquareSize();
     }
 
     public static int getSecNum(Customer customer, Pinpad pinpad) {
@@ -20,8 +20,6 @@ public class SecretNumberRefactor2 {
         int rowSec = (pinpad.getSquareSize() + rowMid + rowMov) % pinpad.getSquareSize();
         int colSec = (pinpad.getSquareSize() + colMid + colMov) % pinpad.getSquareSize();
 
-        int secNumPos = pinpad.get(rowSec * pinpad.getSquareSize() + colSec);
-
-        return pinpad.get(secNumPos);
+        return pinpad.get(rowSec * pinpad.getSquareSize() + colSec);
     }
 }
--- src/test/java/SecretNumberRefactor2Test.java
+++ src/test/java/SecretNumberRefactor2Test.java
@@ -209,4 +209,81 @@ public class SecretNumberRefactor2Test {
         customer.setDirectionNumber(1);
         Assert.assertEquals(25, SecretNumberRefactor2.getSecNum(customer, pinpad));
     }
+
+    @Test
+    public void secretNumberCanBeFoundOnShuffledPinpads() {
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+
+        int[] customIntArray = new int[]{25, 43, 11, 35, 38, 34, 30, 23, 44, 3, 4, 14, 32, 9, 22, 40, 45, 29, 26, 21, 36, 15, 8, 7, 16, 47, 42, 2, 39, 13, 6, 10, 24, 1, 37, 17, 48, 20, 31, 19, 41, 33, 5, 12, 18, 0, 27, 28, 46};
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(customIntArray[i]);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+        Assert.assertEquals(32, SecretNumberRefactor2.getSecNum(customer,pinpad));
+    }
+
+    @Test
+    public void secretNumberCanBeFoundOnShuffledPinpadsMoreTests01() {
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+
+        int[] customIntArray = new int[]{28,40,11,30,3,17,37,21,15,7,20,8,16,45,12,19,14,42,31,5,26,35,34,29,10,9,18,23,4,2,46,1,24,0,25,33,48,38,36,39,6,22,44,13,27,43,32,47,41};
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(customIntArray[i]);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+        Assert.assertEquals(34, SecretNumberRefactor2.getSecNum(customer,pinpad));
+    }
+
+    @Test
+    public void secretNumberCanBeFoundOnShuffledPinpadsMoreTests02() {
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+
+        int[] customIntArray = new int[]{13,16,0,39,21,9,36,47,3,7,32,15,5,42,28,19,38,26,43,18,31,23,35,27,25,8,44,30,45,17,11,48,41,14,20,1,4,2,10,6,40,22,34,33,46,37,12,29,24};
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(customIntArray[i]);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+        Assert.assertEquals(3, SecretNumberRefactor2.getSecNum(customer,pinpad));
+    }
+
+    @Test
+    public void secretNumberCanBeFoundOnShuffledPinpadsMoreTests03() {
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+
+        int[] customIntArray = new int[]{11,10,21,39,14,4,27,36,38,45,5,20,22,24,31,6,18,1,23,0,35,42,41,34,15,2,48,28,30,33,7,32,37,29,43,13,26,16,8,3,19,46,17,44,12,47,40,25,9};
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(customIntArray[i]);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+        Assert.assertEquals(41, SecretNumberRefactor2.getSecNum(customer,pinpad));
+    }
+
 }
