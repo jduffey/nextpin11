a9a8a97471cff4ab5e47ed09de9bde46c1a5f9a9
Jed Duffey <jedduffey@gmail.com>
2018-07-29 21:43:04 -0400

    More code...

--- src/main/java/SecretNumberRefactor2.java
+++ src/main/java/SecretNumberRefactor2.java
@@ -9,4 +9,19 @@ public class SecretNumberRefactor2 {
     public static int getVerMove(Customer customer, Pinpad pinpad) {
         return customer.getDirectionNumber() / pinpad.getSquareSize() - customer.getAnchorNumber() / pinpad.getSquareSize();
     }
+
+    public static int getSecNum(Customer customer, Pinpad pinpad) {
+
+        int midNum = pinpad.getArraySizeOfPinPad() / 2;
+        int rowMov = getHorMove(customer, pinpad);
+        int colMov = getVerMove(customer, pinpad);
+
+        int rowMid = pinpad.getSquareSize() / 2;
+        int colMid = pinpad.getSquareSize() / 2;
+
+        int secNum = midNum + rowMov;
+
+        return secNum;
+
+    }
 }
--- src/test/java/SecretNumberRefactor2Test.java
+++ src/test/java/SecretNumberRefactor2Test.java
@@ -101,4 +101,29 @@ public class SecretNumberRefactor2Test {
         Assert.assertEquals(-4, SecretNumberRefactor2.getVerMove(customer, pinpad));
     }
 
+    @Test
+    public void secretNumberCanBeFoundGivenOnlyHorizontalMovement() {
+        Pinpad pinpad = new Pinpad(7);
+        Customer customer = new Customer();
+        ArrayList customArray = new ArrayList();
+
+        for (int i = 0; i < pinpad.getSquareSize() * pinpad.getSquareSize(); i++) {
+            customArray.add(i);
+        }
+        pinpad.setCustomArray(pinpad.getSquareSize(), customArray);
+        Displayer.displayPinpadSquare(pinpad);
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(1);
+        Assert.assertEquals(25, SecretNumberRefactor2.getSecNum(customer, pinpad));
+
+        customer.setAnchorNumber(0);
+        customer.setDirectionNumber(2);
+        Assert.assertEquals(26, SecretNumberRefactor2.getSecNum(customer, pinpad));
+
+//        customer.setAnchorNumber(42);
+//        customer.setDirectionNumber(46);
+//        Assert.assertEquals(21, SecretNumberRefactor2.getSecNum(customer, pinpad));
+    }
+
 }
