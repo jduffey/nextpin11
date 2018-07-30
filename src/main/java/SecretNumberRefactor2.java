33cb72c375c60ced5fc31f458def3bf424958e47
Jed Duffey <jedduffey@gmail.com>
2018-07-29 22:27:34 -0400

    First two methods are named incorrectly; clean up unused code

--- src/main/java/SecretNumberRefactor2.java
+++ src/main/java/SecretNumberRefactor2.java
@@ -1,22 +1,19 @@
 public class SecretNumberRefactor2 {
 
 
+    // getVerMove is named wrong
     public static int getVerMove(Customer customer, Pinpad pinpad) {
 
         return customer.getDirectionNumber() % pinpad.getSquareSize() - customer.getAnchorNumber() % pinpad.getSquareSize();
     }
 
+    // getHorMove is named wrong
     public static int getHorMove(Customer customer, Pinpad pinpad) {
         return customer.getDirectionNumber() / pinpad.getSquareSize() - customer.getAnchorNumber() / pinpad.getSquareSize();
     }
 
-    public static int getRowSec(Customer customer, Pinpad pinpad) {
-        return 999;
-    }
-
     public static int getSecNum(Customer customer, Pinpad pinpad) {
 
-        //int midNum = pinpad.getArraySizeOfPinPad() / 2;
         int rowMov = getHorMove(customer, pinpad);
         int colMov = getVerMove(customer, pinpad);
 
@@ -29,6 +26,5 @@ public class SecretNumberRefactor2 {
         int secNum = pinpad.get(rowSec * pinpad.getSquareSize() + colSec);
 
         return secNum;
-
     }
 }
