d8ace0e10c0ffdb98d814c7e92097d3ba44249ef
Jed Duffey <jedduffey@gmail.com>
2018-07-29 22:41:18 -0400

    Update class/method references

--- src/main/java/Displayer.java
+++ src/main/java/Displayer.java
@@ -36,6 +36,6 @@ public class Displayer {
 
     public static void displaySecretNumber(Customer customer, Pinpad pinpad) {
 
-        System.out.println(SecretNumberRefactor.findSecretNumber(customer, pinpad));
+        System.out.println(SecretNumberRefactor2.getSecNum(customer, pinpad));
     }
 }
