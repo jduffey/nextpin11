242f2f4ab114a87e2d4adf1625c7ee9b60779857
Jed Duffey <jedduffey@gmail.com>
2018-08-05 21:19:44 -0400

    User can set anchor and directions numbers

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
         Pinpad pinpad = new Pinpad(7);
         Customer customer = new Customer();
-        customer.setAnchorNumber(0);
-        customer.setDirectionNumber(1);
 
         Displayer.displayIntroMessage(pinpad);
+
+        askForAndSetPrivateNumbers(customer);
 
         Displayer.displayAnchorAndDirectionNumber(customer);
 
@@ -17,5 +19,19 @@ public class App {
 
         Displayer.displaySecretNumber(customer, pinpad);
 
+    }
+
+    private static void askForAndSetPrivateNumbers(Customer customer) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set anchor number: ");
+        int ancNum = scanner.nextInt();
+        customer.setAnchorNumber(ancNum);
+
+        System.out.println("Set direction number: ");
+        int dirNum = scanner.nextInt();
+        customer.setDirectionNumber(dirNum);
+
+        System.out.println("Thank you.");
     }
 }
