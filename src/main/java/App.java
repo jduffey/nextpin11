9c8a7a4c325b308a04786f2295a9f464a5bc582d
Jed Duffey <jedduffey@gmail.com>
2018-08-06 23:25:44 -0400

    Refactor logic out of Main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,42 +1,14 @@
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
 
-        Customer customer = establishCustomer();
+        Customer customer = new Customer();
+
+        customer.askCustomerForSettings();
 
         LoginSessionAttempt session = new LoginSessionAttempt(customer);
 
         session.initiateLoginAttempt();
-    }
-
-    private static Customer establishCustomer() {
-        Customer customer = new Customer();
-        setCustomerSettings(customer);
-        return customer;
-    }
-
-    private static void setCustomerSettings(Customer customer) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("--ESTABLISH CUSTOMER ACCOUNT--");
-        System.out.println("Please enter your name: ");
-        customer.setName(scanner.next());
-
-        System.out.println("Pinpad size must be an odd number");
-        System.out.println("between 3 and 31 (inclusive).");
-        System.out.println("Enter preferred pinpad size: ");
-        customer.setPreferredPinpadSize(scanner.nextInt());
-
-        System.out.println("Set anchor number: ");
-        customer.setAnchorNumber(scanner.nextInt());
-
-        System.out.println("Set direction number: ");
-        customer.setDirectionNumber(scanner.nextInt());
-
-        System.out.println("Thank you for following the instructions.");
     }
 
 }
--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Customer {
 
     private int anchorNumber;
@@ -33,7 +35,29 @@ public class Customer {
         this.preferredPinpadSize = preferredPinpadSize;
     }
 
-    public int getPreferredPinpadSize(){
+    public int getPreferredPinpadSize() {
         return preferredPinpadSize;
+    }
+
+    public void askCustomerForSettings() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--ESTABLISH CUSTOMER ACCOUNT--");
+        System.out.println("Please enter your name: ");
+        setName(scanner.next());
+
+        System.out.println("Pinpad size must be an odd number");
+        System.out.println("between 3 and 31 (inclusive).");
+        System.out.println("Enter preferred pinpad size: ");
+        setPreferredPinpadSize(scanner.nextInt());
+
+        System.out.println("Set anchor number: ");
+        setAnchorNumber(scanner.nextInt());
+
+        System.out.println("Set direction number: ");
+        setDirectionNumber(scanner.nextInt());
+
+        System.out.println("Thank you for following the instructions.");
     }
 }
